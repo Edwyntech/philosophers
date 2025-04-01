80ad727949ad2c25418b2bd0f30a443dbacb0bd5
nicolas <nicolas.seiller@protonmail.com>
2025-04-01 12:02:31 +0200

    Throw exceptions when misusing chopsticks

--- src/main/java/tech/edwyn/philosophers/Chopstick.java
+++ src/main/java/tech/edwyn/philosophers/Chopstick.java
@@ -8,10 +8,16 @@ public class Chopstick {
     }
 
     public void take() {
+        if (!this.isAvailable) {
+            throw new IllegalStateException("Chopstick is not available and cannot be taken.");
+        }
         this.isAvailable = false;
     }
 
     public void putDown() {
+        if (this.isAvailable) {
+            throw new IllegalStateException("Chopstick is not taken and cannot be put down.");
+        }
         this.isAvailable = true;
     }
 }
--- src/test/java/tech/edwyn/philosophers/ChopstickTest.java
+++ src/test/java/tech/edwyn/philosophers/ChopstickTest.java
@@ -1,30 +1,49 @@
 package tech.edwyn.philosophers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ChopstickTest {
 
+
+    private Chopstick chopstick;
+
+    @BeforeEach
+    public void initTest() {
+        chopstick = new Chopstick();
+    }
+
     @Test
     void shouldBeAvailableAtInitialization() {
-        Chopstick chopstick = new Chopstick();
         assertThat(chopstick.isAvailable()).isTrue();
     }
 
     @Test
     void shouldNotBeAvailableWhenTaken() {
-        Chopstick chopstick = new Chopstick();
         chopstick.take();
         assertThat(chopstick.isAvailable()).isFalse();
     }
 
     @Test
     void shouldBeAvailableAgainWhenPutDown() {
-        Chopstick chopstick = new Chopstick();
         chopstick.take();
         chopstick.putDown();
         assertThat(chopstick.isAvailable()).isTrue();
     }
 
+    @Test
+    void shouldErrorOutIfTakenWhenNotAvailable() {
+        chopstick.take();
+        assertThatThrownBy(() -> chopstick.take()).isInstanceOf(IllegalStateException.class).hasMessage(
+                "Chopstick is not available and cannot be taken.");
+    }
+
+    @Test
+    void shouldErrorOutIfPutDownWhenAvailable() {
+        assertThatThrownBy(() -> chopstick.putDown()).isInstanceOf(IllegalStateException.class).hasMessage(
+                "Chopstick is not taken and cannot be put down.");
+    }
 }
